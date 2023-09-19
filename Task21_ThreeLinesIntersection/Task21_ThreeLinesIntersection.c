41649ad5e72b8a7470d9f47e800d9b0d362d9dee
EAndPlay <andplayyt@gmail.com>
2023-09-19 15:36:52 +0300

    Final

--- Task21_ThreeLinesIntersection/Task21_ThreeLinesIntersection.c
+++ Task21_ThreeLinesIntersection/Task21_ThreeLinesIntersection.c
@@ -1,4 +1,6 @@
-﻿#include <stdio.h>
+﻿//Лабораторная работа по программированию №1. "Пересечение трёх прямых" Вариант №21. Гонцов А.М 1-43
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <locale.h>
@@ -20,9 +22,9 @@ typedef line;
 struct tagLINEFUNCTION
 {
     line ready_line;
-    float k; //coefficient
-    float b; //free coefficient
-    unsigned char flag; //is vertical
+    float k; //коэффициент
+    float b; //свободный член
+    unsigned char flag; //является ли прямая вертикальной
 }
 typedef line_function;
 
@@ -34,12 +36,16 @@ struct tagINTERSECTION_POINT
 }
 typedef intersection_point;
 
-inline void input_line_coords(line* target_line, const char* pre_input_text);
+//Запрашивает коодинаты двух точек прямой
+void input_line_coords(line* target_line, const char* pre_input_text);
 
+//Высчитывает коэффициенты прямых k и b
 void init_lf(line_function* lf);
 
+//Находит точку пересечения двух прямых
 point find_intersection_point(line_function* lf1, line_function* lf2);
 
+//Находит все точки пересечения данных прямых
 intersection_point* find_intersections_points(line_function** lfs_array);
 
 unsigned char check_lfs_validation(line_function** lfs_array);
@@ -54,22 +60,22 @@ int main()
     all_lfs[0] = &lf1;
     all_lfs[1] = &lf2;
     all_lfs[2] = &lf3;
-    printf("Формат ввода координат точек прямой: X1 Y1 X2 Y2\n");
+    printf("Формат ввода координат точек прямой: X1 Y1 X2 Y2 (Пример: 1 1 2 2)\n");
     input_line_coords(&lf1.ready_line, "Первая прямая: ");
     input_line_coords(&lf2.ready_line, "Вторая прямая: ");
     input_line_coords(&lf3.ready_line, "Третья прямая: ");
     init_lf(&lf1);
     init_lf(&lf2);
     init_lf(&lf3);
+
+    //Проверка на то, смогут ли далее прямые пересечься
     if (!check_lfs_validation(all_lfs))
     {
         printf("Невозможно найти все пересечения\n");
         system("pause");
         return;
     }
-    point intersect1 = find_intersection_point(&lf1, &lf2);
-    point intersect2 = find_intersection_point(&lf1, &lf3);
-    point intersect3 = find_intersection_point(&lf2, &lf3);
+
     intersection_point* intersections = find_intersections_points(all_lfs);
     int intersections_count = _msize(intersections) / sizeof(intersection_point);
     if (intersections_count == 1)
@@ -79,6 +85,7 @@ int main()
     else
     {
         printf("Не все прямые пересекаются в одной точке\n");
+        //Выводит все точки в которых пересекаются прямые
         for (int i = 0; i < intersections_count; i++)
         {
             intersection_point inter_point = intersections[i];
@@ -91,31 +98,40 @@ int main()
 void input_line_coords(line* target_line, const char* pre_input_text)
 {
     printf(pre_input_text);
-    scanf_s("%f %f %f %f", &target_line->p1.x, &target_line->p1.y, &target_line->p2.x, &target_line->p2.y);
+    if (!scanf_s("%f %f %f %f", &target_line->p1.x, &target_line->p1.y, &target_line->p2.x, &target_line->p2.y))
+    {
+        printf("Неправильный тип введённых данных\n");
+        system("pause");
+        exit(0);
+    }
 }
 
 void init_lf(line_function* lf)
 {
     point *p1 = &lf->ready_line.p1, *p2 = &lf->ready_line.p2;
-    int delta_x = p1->x - p2->x;
-
-    lf->k = delta_x != 0 ? fabs((p2->y - p1->y) / delta_x) : 0;
-    if ((p1->x < p2->x && p2->y < p1->y)
-        || (p1->x > p2->x && p1->y < p2->y))
-    {
-        lf->k *= -1;
-    }
-
-    lf->b = p1->y - lf->k * p1->x;
-
+    //При одинаковой X координате и различными Y прямая помечается как вертикальная
     if (p1->x == p2->x && p1->y != p2->y)
     {
         lf->flag = 1;
+        lf->k = 0;
+        lf->b = 0;
+        return;
     }
     else
     {
         lf->flag = 0;
     }
+    int delta_x = p1->x - p2->x;
+
+    lf->k = delta_x != 0 ? fabs((p2->y - p1->y) / delta_x) : 0;
+    if (lf->k 
+        && ((p1->x < p2->x && p2->y < p1->y) 
+            || (p1->x > p2->x && p1->y < p2->y)))
+    {
+        lf->k *= -1;
+    }
+
+    lf->b = p1->y - lf->k * p1->x;
 }
 
 point find_intersection_point(line_function* lf1, line_function* lf2)
@@ -139,6 +155,7 @@ point find_intersection_point(line_function* lf1, line_function* lf2)
     }
     else
     {
+        //Высчитывание координат пересечения по формуле y = k * x + b
         x = (lf1->b - lf2->b) / (lf2->k - lf1->k);
         y = lf1->k * x + lf1->b;
     }
@@ -157,6 +174,7 @@ intersection_point* find_intersections_points(line_function** lfs_array)
         {
             point intersection = find_intersection_point(lfs_array[i], lfs_array[j]);
 
+            //Проверка на наличие точки с такими же координатами, если такая есть, то точка пропускается
             unsigned char is_copy = 0;
             if (result_size != 0)
             {
@@ -173,6 +191,7 @@ intersection_point* find_intersections_points(line_function** lfs_array)
             if (is_copy) continue;
 
             result_size++;
+            //Запись новой точки пересечения в список
             intersections = realloc(intersections, sizeof(intersection_point) * result_size);
             intersection_point inter_point = { intersection, i + 1, j + 1 };
             intersections[result_size - 1] = inter_point;
@@ -189,9 +208,11 @@ unsigned char check_lfs_validation(line_function** lfs_array)
     {
         line_function* i_lf = lfs_array[i];
         line* i_line = &i_lf->ready_line;
+        //Проверка на равность точек
         if (*(long long*)&i_line->p1 == *(long long*)&i_line->p2)
         {
             printf("Прямая %d не существует\n", i + 1);
+            result = 0;
             continue;
         }
         for (int j = i + 1; j < array_length; j++)
@@ -199,6 +220,7 @@ unsigned char check_lfs_validation(line_function** lfs_array)
             line_function* j_lf = lfs_array[j];
             line* j_line = &j_lf->ready_line;
             char* warning_str = 0;
+            //Проверка на равность прямых
             if (*(long long*)&i_line->p1 == *(long long*)&j_line->p1
                 && *(long long*)&i_line->p2 == *(long long*)&j_line->p2)
             {
