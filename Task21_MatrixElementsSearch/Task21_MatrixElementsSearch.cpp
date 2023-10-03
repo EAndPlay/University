eb1c494febc66b0f70420451fa177f715c1c8d8b
EAndPlay <andplayyt@gmail.com>
2023-10-03 14:05:06 +0300

    Some fixes

--- Task21_MatrixElementsSearch/Task21_MatrixElementsSearch.cpp
+++ Task21_MatrixElementsSearch/Task21_MatrixElementsSearch.cpp
@@ -16,16 +16,6 @@ int main()
 	std::cout << "Введите двумерный массив: (для прекращения ввода - на пустой строке нажмите Enter)" << std::endl;
 	int** two_d_array = get_2d_matrix();
 
-	//std::cout << "size: " << _msize(one_d_array) / sizeof(int) << std::endl;
-	//for (int i = 0; i < _msize(two_d_array) / sizeof(size_t); i++)
-	//{
-	//	for (int j = 0; j < _msize(two_d_array[i]) / sizeof(int); j++)
-	//	{
-	//		std::cout << two_d_array[i][j] << " ";
-	//	}
-	//	std::cout << std::endl;
-	//}
-
 	for (int i = 0; i < _msize(one_d_array) / sizeof(int); i++)
 	{
 		int first_element = one_d_array[i];
@@ -53,6 +43,8 @@ int main()
 			std::cout << "Пара не найдена";
 		std::cout << std::endl;
 	}
+
+	system("pause");
 }
 
 int* get_array_elements_row()
@@ -61,17 +53,15 @@ int* get_array_elements_row()
 	int i = 0;
 	while (true)
 	{
-		//std::cout << "peek: " << std::cin.peek() << std::endl;
 		if (std::cin.peek() == '\n') break;
-		array = (int*)realloc(array, (i + 1) * sizeof(int));
+		array = (int*) realloc(array, (i + 1) * sizeof(int));
 		std::cin >> array[i];
 
 		if (std::cin.fail())
 		{
-			std::cout << "Неправильный тип введённых данных, для повтора начните всё заного" << std::endl;
-			free(array);
-			array = get_array_elements_row();
-			return array;
+			std::cout << "Введён неправильный тип данных, для повтора начните всё заного" << std::endl;
+			system("pause");
+			exit(0);
 		}
 		i++;
 	}
@@ -87,7 +77,7 @@ int** get_2d_matrix()
 	{
 		std::cout << i + 1 << ": ";
 		if (std::cin.peek() == '\n') break;
-		array = (int** )realloc(array, (i + 1) * sizeof(size_t));
+		array = (int**) realloc(array, (i + 1) * sizeof(size_t));
 		array[i] = get_array_elements_row();
 		i++;
 	}
