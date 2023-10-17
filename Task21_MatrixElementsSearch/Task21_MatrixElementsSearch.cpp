dbafc0bd3b83bf8c502188c7068f86cb1c534cdb
EAndPlay <andplayyt@gmail.com>
2023-10-17 03:53:56 +0300

    Filter for no copies of values in array

--- Task21_MatrixElementsSearch/Task21_MatrixElementsSearch.cpp
+++ Task21_MatrixElementsSearch/Task21_MatrixElementsSearch.cpp
@@ -13,18 +13,36 @@ int main()
 	std::cout << "Введите одномерный массив: ";
 	int* one_d_array = get_array_elements_row();
 
+	int one_d_length = _msize(one_d_array) / sizeof(int);
+
+	if (!one_d_length)
+	{
+		std::cout << "Одномерная матрица пуста.";
+		return 0;
+	}
+
 	std::cout << "Введите двумерный массив: (для прекращения ввода - на пустой строке нажмите Enter)" << std::endl;
 	int** two_d_array = get_2d_matrix();
 
-	for (int i = 0; i < _msize(one_d_array) / sizeof(int); i++)
+	int two_d_length = _msize(two_d_array) / sizeof(size_t);
+
+	if (!two_d_length)
+	{
+		std::cout << "Двумерная матрица пуста.";
+		return 0;
+	}
+
+	for (int i = 0; i < one_d_length; i++)
 	{
 		int first_element = one_d_array[i];
 		bool is_found = false;
 		std::cout << i + 1 << " : ";
 
-		for (int j = 0; j < _msize(two_d_array) / sizeof(size_t); j++)
+		for (int j = 0; j < two_d_length; j++)
 		{
-			for (int k = 0; k < _msize(two_d_array[j]) / sizeof(int); k++)
+			int one_d_element_length = _msize(two_d_array[j]) / sizeof(int);
+
+			for (int k = 0; k < one_d_element_length; k++)
 			{
 				int second_element = two_d_array[j][k];
 
@@ -49,7 +67,7 @@ int main()
 
 int* get_array_elements_row()
 {
-	int* array = (int*)malloc(sizeof(int));
+	int* array = (int*)malloc(1);
 	int i = 0;
 	while (true)
 	{
@@ -65,13 +83,40 @@ int* get_array_elements_row()
 		}
 		i++;
 	}
+
+	int index = -1;
+	//Исключает повторяющиеся значения
+
+	//Для рекурсии
+	while (true)
+	{
+		bool flag = false;
+		for (int j = 0; j < i; j++)
+		{
+			int j_element = array[j];
+			for (int k = j + 1; k < i; k++)
+			{
+				if (j_element == array[k])
+				{
+					int* new_array = (int*) calloc(i - 1, sizeof(int));
+					memcpy(new_array, array, k * sizeof(int));
+					memcpy(new_array + k, array + k + 1, (i - k) * sizeof(int));
+					i--;
+					flag = true;
+					free(array);
+					array = new_array;
+				}
+			}
+		}
+		if (!flag) break;
+	}
 	std::cin.get();
 	return array;
 }
 
 int** get_2d_matrix()
 {
-	int** array = (int**) malloc(sizeof(size_t));
+	int** array = (int**) malloc(1);
 	int i = 0;
 	while (true)
 	{
